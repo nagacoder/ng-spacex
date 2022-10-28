c487d93461b032340edc248084c692c208b4368c
nagacoder <nagacoder@outlook.com>
2022-10-28 16:08:36 +0700

    feat: integrate dragon api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { DragonComponent } from './pages/dragon/dragon.component';
 import { LayoutsComponent } from './components/layouts/layouts.component';
@@ -24,7 +24,7 @@ import { InfoComponent } from './pages/info/info.component';
     MissionComponent,
     InfoComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/pages/dragon/dragon.component.ts
+++ src/app/pages/dragon/dragon.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../api.service';
 
 @Component({
   selector: 'app-dragon',
   templateUrl: './dragon.component.html',
-  styleUrls: ['./dragon.component.scss']
+  styleUrls: ['./dragon.component.scss'],
 })
 export class DragonComponent implements OnInit {
-
-  constructor() { }
+  dragons: any = [];
+  constructor(private api: ApiService) {}
 
   ngOnInit(): void {
+    this.api.getDragons().subscribe((data: any) => {
+      this.dragons = data;
+      console.log(this.dragons);
+    });
   }
 
+  renderDescription(description: string) {
+    return description.slice(0, 300) + '...';
+  }
 }
